0369ce6cd4e8b93cdd9f06c484b26eb860ce515f
orb <orb@orbatron.org>
2026-03-11 14:57:56 -0400

    footer buttons spaced out

--- packages/react-app/src/common/Footer/FooterDrawer.tsx
+++ packages/react-app/src/common/Footer/FooterDrawer.tsx
@@ -88,14 +88,14 @@ const FooterDrawer = observer(() => {
                   <FooterItem title={"$DOG"} items={tradeLinks} />
                 </Grid>
               </Box>
-              <HStack pt={4} pb={2} spacing={3} flexWrap="wrap">
-                <Button size="xs" onClick={() => { AppStore.guide.startGuide(); setIsOpen(false); }}>
+              <HStack pt={4} pb={2} spacing={6} flexWrap="wrap">
+                <Button size="xs" px={4} py={2} onClick={() => { AppStore.guide.startGuide(); setIsOpen(false); }}>
                   Run Interactive User Guide
                 </Button>
-                <Button size="xs" onClick={() => { setIsBugReportOpen(true); setIsOpen(false); }}>
+                <Button size="xs" px={4} py={2} onClick={() => { setIsBugReportOpen(true); setIsOpen(false); }}>
                   Find a bug?!
                 </Button>
-                <Button size="xs" onClick={() => { setIsCreditsOpen(true); setIsOpen(false); }}>
+                <Button size="xs" px={4} py={2} onClick={() => { setIsCreditsOpen(true); setIsOpen(false); }}>
                   Credits
                 </Button>
               </HStack>
